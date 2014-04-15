96f1e6d66e06207bd34af481fac5642409f69b9f
Steven Bambling <smbambling@gmail.com>
2014-04-15 06:38:54 -0400

    adding symlink for check_fping command script

--- ZenPacks/community/fping/__init__.py
+++ ZenPacks/community/fping/__init__.py
@@ -1,8 +1,29 @@
 
+import os
 import Globals
-import os.path
+from Products.ZenModel.ZenPack import ZenPack as ZenPackBase
+from Products.ZenUtils.Utils import zenPath
+from Products.CMFCore.DirectoryView import registerDirectory
 
 skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
 from Products.CMFCore.DirectoryView import registerDirectory
 if os.path.isdir(skinsDir):
     registerDirectory(skinsDir, globals())
+
+class ZenPack(ZenPackBase):
+    """
+    ZenPack class to add new zProperties and perform other installation and
+    removal tasks.
+    """
+    def install(self, app):
+        super(ZenPack, self).install(app)
+        self.symlinkPlugin()
+    def remove(self, app, leaveObjects=False):
+        if not leaveObjects:
+            self.removePluginSymlink()
+        super(ZenPack, self).remove(app, leaveObjects=leaveObjects)
+    def symlinkPlugin(self):
+        os.system('ln -sf %s/check_fping %s/' %
+            (self.path('libexec'), zenPath('libexec')))
+    def removePluginSymlink(self):
+        os.system('rm -f %s/check_fping' % (zenPath('libexec')))
